3b156d61f636e2470bf70326737c3e1edb92eece
TettaTakahashi <x4tetta@gmail.com>
2024-05-11 02:30:01 +0000

    [Update]Jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
   root to: "event#index"
+  
+  get '/event', to: 'event#index', defaults: { format: 'json' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
